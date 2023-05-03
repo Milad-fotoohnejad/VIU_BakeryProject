f26550caaf1d542f26947067fbdcda6d2bf416af
Milad Fotoohnejad <milad.fotoohnejad@itas.ca>
2023-05-02 21:44:23 -0700

    Separated the bread and pastry functions and their build widgets

--- viu_bakery/lib/recipe_upload_page.dart
+++ viu_bakery/lib/recipe_upload_page.dart
@@ -19,125 +19,132 @@ class _RecipeUploadPageState extends State<RecipeUploadPage> {
         .pickFiles(type: FileType.custom, allowedExtensions: ['xlsx']);
     if (result != null) {
       Uint8List fileBytes = result.files.single.bytes!;
-      String jsonArray = _convertExcelToJson(fileBytes);
+      List<Map<String, dynamic>> jsonArray = _convertExcelToJson(
+          fileBytes); // Change this to List<Map<String, dynamic>>
       setState(() {
-        _jsonArray = jsonArray;
+        _jsonArray =
+            jsonEncode(jsonArray); // Encode the jsonArray to JSON string here
       });
     }
   }
 
-  String _convertExcelToJson(Uint8List bytes) {
+  Map<String, dynamic> _convertPastryToJson(List<List<dynamic>> rows) {
+    Map<String, dynamic> recipe = {};
+
+    int ingredientsStartRow = 10;
+    int methodStartRow = 26;
+
+    for (int i = 0; i < rows.length; i++) {
+      if (rows[i][0] != null && rows[i][0].toString().contains('Pastry')) {
+        recipe = {
+          'category': rows[3][0].toString().split(': ')[1].trim(),
+          'yield': rows[3][5],
+          'name': rows[5][0].toString().split(': ')[1].trim(),
+          'unitWeight': rows[5][4].toString().trim(),
+        };
+
+        List<Map<String, dynamic>> formula = [];
+        for (int j = ingredientsStartRow; j < rows.length; j++) {
+          if (rows[j][0] != null && rows[j][0].toString().contains('TOTAL')) {
+            break;
+          }
+          formula.add({
+            'ingredient': rows[j][0],
+            'qty': rows[j][1],
+            'unit': rows[j][2],
+            'multiplier': rows[j][3],
+            'bakersPercentage': rows[j][5],
+          });
+        }
+        recipe['formula'] = formula;
+
+        List<String> method = [];
+        for (int j = methodStartRow; j < rows.length; j++) {
+          if (rows[j][0] == null) {
+            break;
+          }
+          method.add(rows[j][0].toString().trim());
+        }
+        recipe['method'] = method;
+
+        break;
+      }
+    }
+
+    return recipe;
+  }
+
+  Map<String, dynamic> _convertBreadToJson(List<List<dynamic>> rows) {
+    var category = rows[3][0]?.split(': ')[1] ?? '';
+    var yieldValue = rows[3][5] ?? 0;
+    var ddt = rows[4][4]?.split(': ')[1] ?? '0';
+    var name = rows[5][0]?.split(': ')[1] ?? '';
+    var scalingWeight = rows[5][4]?.split(': ')[1] ?? '0';
+
+    var formula = <Map<String, dynamic>>[];
+
+    for (int i = 10; i < rows.length; i++) {
+      if (rows[i][0] == 'Total' || rows[i][0] == 'Method' || rows[i][0] == null)
+        break;
+
+      if (rows[i].length < 7) {
+        rows[i].length = 7;
+        for (int j = rows[i].length; j < 7; j++) {
+          rows[i].add(null);
+        }
+      }
+
+      formula.add({
+        'ingredient': rows[i][0] ?? '',
+        'starter': rows[i][1] ?? 0,
+        'dough': rows[i][3] ?? 0, // Change this from rows[i][2] to rows[i][3]
+        'bakersPercentage': (rows[i][5] ?? 0) /
+            100, // Change this from rows[i][4] to rows[i][5] and divide by 100
+        'overallFormula': rows[i][6] ?? 0,
+      });
+    }
+
+    int methodStartRow = rows.indexWhere((row) => row[0] == 'Method');
+    var method = <String>[];
+
+    for (int i = methodStartRow + 1; i < rows.length; i++) {
+      if (rows[i] == null)
+        break; // Add a check to see if the row starts with a number followed by a period
+      if (rows[i][0] != null &&
+          rows[i][0] != '' &&
+          RegExp(r'^\d+\.\s').hasMatch(rows[i][0])) {
+        method.add(rows[i][0] ?? '');
+      }
+    }
+
+    return {
+      'category': category,
+      'yield': yieldValue,
+      'ddt': ddt,
+      'name': name,
+      'scalingWeight': scalingWeight,
+      'formula': formula,
+      'method': method,
+    };
+  }
+
+  List<Map<String, dynamic>> _convertExcelToJson(Uint8List bytes) {
     var decoder = SpreadsheetDecoder.decodeBytes(bytes);
     var sheet = decoder.tables.values.first;
     var rows = sheet.rows;
 
-    bool isNewType =
-        rows[2][2] != null && rows[2][2].toString().contains('Pastry');
+    bool pastryType = rows[6]
+        .any((cell) => cell != null && cell.toString().contains('Pastry'));
+
     List<Map<String, dynamic>> recipes = [];
 
-    if (isNewType) {
-      var category = rows[3][0]?.split(': ')[1]?.trim() ?? '';
-      var yieldValue = rows[3][5] ?? 0;
-      var name = rows[5][0]?.split(': ')[1]?.trim() ?? '';
-      var unitWeight = rows[5][4]?.split(': ')[1]?.trim() ?? '';
-
-      var formula = <Map<String, dynamic>>[];
-
-      for (int i = 10; i < rows.length; i++) {
-        if (rows[i][0] == 'Total' ||
-            rows[i][0] == 'Method' ||
-            rows[i][0] == null) break;
-
-        formula.add({
-          'ingredient': rows[i][0] ?? '',
-          'qty': _parseCellValue(rows[i][1]) ?? 0,
-          'unit': rows[i][2] ?? '',
-          'bakersPercentage': _parseCellValue(rows[i][5]) ?? 0,
-        });
-      }
-
-      int totalWeightRow = rows.indexWhere((row) => row[0] == 'Total');
-      int totalMultiplier = _parseCellValue(rows[totalWeightRow][4]);
-
-      int methodStartRow = rows.indexWhere((row) => row[0] == 'Method');
-      var method = <String>[];
-
-      for (int i = methodStartRow + 1; i < rows.length; i++) {
-        if (rows[i] == null) break;
-        if (rows[i][0] != null && rows[i][0] != '') {
-          method.add(rows[i][0] ?? '');
-        }
-      }
-
-      Map<String, dynamic> recipe = {
-        'name': name,
-        'category': category,
-        'yield': yieldValue,
-        'unitWeight': unitWeight,
-        'formula': formula,
-        'total': {
-          'weight': _parseCellValue(rows[totalWeightRow][1]),
-          'multiplier': totalMultiplier,
-        },
-        'method': method,
-      };
-      recipes.add(recipe);
+    if (pastryType) {
+      recipes.add(_convertPastryToJson(rows));
     } else {
-      var category = rows[3][0]?.split(': ')[1] ?? '';
-      var yieldValue = rows[3][5] ?? 0;
-      var ddt = rows[4][4]?.split(': ')[1] ?? '0';
-      var name = rows[5][0]?.split(': ')[1] ?? '';
-      var scalingWeight = rows[5][4]?.split(': ')[1] ?? '0';
-
-      var formula = <Map<String, dynamic>>[];
-
-      for (int i = 10; i < rows.length; i++) {
-        if (rows[i][0] == 'Total' ||
-            rows[i][0] == 'Method' ||
-            rows[i][0] == null) break;
-
-        if (rows[i].length < 7) {
-          rows[i].length = 7;
-          for (int j = rows[i].length; j < 7; j++) {
-            rows[i].add(null);
-          }
-        }
-
-        formula.add({
-          'ingredient': rows[i][0] ?? '',
-          'starter': rows[i][1] ?? 0,
-          'dough': rows[i][3] ?? 0, // Change this from rows[i][2] to rows[i][3]
-          'bakersPercentage': (rows[i][5] ?? 0) /
-              100, // Change this from rows[i][4] to rows[i][5] and divide by 100
-          'overallFormula': rows[i][6] ?? 0,
-        });
-      }
-
-      int methodStartRow = rows.indexWhere((row) => row[0] == 'Method');
-      var method = <String>[];
-
-      for (int i = methodStartRow + 1; i < rows.length; i++) {
-        if (rows[i] == null)
-          break; // Add a check to see if the row starts with a number followed by a period
-        if (rows[i][0] != null &&
-            rows[i][0] != '' &&
-            RegExp(r'^\d+\.\s').hasMatch(rows[i][0])) {
-          method.add(rows[i][0] ?? '');
-        }
-      }
-
-      Map<String, dynamic> recipe = {
-        'category': category,
-        'yield': yieldValue,
-        'ddt': ddt,
-        'name': name,
-        'scalingWeight': scalingWeight,
-        'formula': formula,
-        'method': method,
-      };
-      recipes.add(recipe);
+      recipes.add(_convertBreadToJson(rows));
     }
-    return jsonEncode(recipes);
+
+    return recipes;
   }
 
   dynamic _parseCellValue(dynamic value) {
@@ -189,7 +196,7 @@ class _RecipeUploadPageState extends State<RecipeUploadPage> {
                   padding: EdgeInsets.all(8.0),
                   child: index < formula.length
                       ? Text(
-                          '${formula[index]['ingredient']}: ${formula[index]['overallFormula']}')
+                          '${formula[index]['ingredient']}: ${formula[index]['dough']}')
                       : SizedBox(),
                 ),
               ),
@@ -255,8 +262,67 @@ class _RecipeUploadPageState extends State<RecipeUploadPage> {
     );
   }
 
-  Column _buildDataTable() {
+  Widget _buildDataTable() {
     List<dynamic> parsedData = jsonDecode(_jsonArray);
+    String recipeType = parsedData[0]['category'];
+
+    if (recipeType.contains('Pastry')) {
+      return _buildPastryDataTable(parsedData);
+    } else {
+      return _buildBreadDataTable(parsedData);
+    }
+  }
+
+  Column _buildPastryDataTable(List<dynamic> parsedData) {
+    List<Map<String, dynamic>> formula =
+        List<Map<String, dynamic>>.from(parsedData[0]['formula']);
+    String category = parsedData[0]['category'];
+    String name = parsedData[0]['name'];
+
+    List<DataRow> rows = formula.map<DataRow>((row) {
+      return DataRow(
+        cells: [
+          DataCell(Text(row['ingredient'])),
+          DataCell(Text(row['qty'].toString())),
+          DataCell(Text(row['unit'])),
+          DataCell(Text(row['multiplier'].toString())),
+          DataCell(Text((row['bakersPercentage'] ?? 0).toString())),
+        ],
+      );
+    }).toList();
+
+    return Column(
+      children: [
+        DataTable(
+          columnSpacing: 24,
+          columns: [
+            DataColumn(label: Text('Category: $category')),
+            DataColumn(label: Text('Name: $name')),
+          ],
+          rows: [
+            DataRow(cells: [
+              DataCell(Text('Yield: ${parsedData[0]['yield']}')),
+              DataCell(Text('Unit Weight: ${parsedData[0]['unitWeight']}')),
+            ]),
+          ],
+        ),
+        SizedBox(height: 16),
+        DataTable(
+          columnSpacing: 24,
+          columns: [
+            DataColumn(label: Text('Ingredients')),
+            DataColumn(label: Text('Qty')),
+            DataColumn(label: Text('Unit')),
+            DataColumn(label: Text('Multiplier')),
+            DataColumn(label: Text('Bakers Percentage')),
+          ],
+          rows: rows,
+        ),
+      ],
+    );
+  }
+
+  Column _buildBreadDataTable(List<dynamic> parsedData) {
     List<Map<String, dynamic>> formula =
         List<Map<String, dynamic>>.from(parsedData[0]['formula']);
     String category = parsedData[0]['category'];
