d3d8e2856b694ba06c7cf93ae159c1d5b9c7e5bd
Milad Fotoohnejad <milad.fotoohnejad@itas.ca>
2023-05-14 14:18:32 -0700

    Added required dependencies and modifications to run the app on the Android

--- viu_bakery/lib/main.dart
+++ viu_bakery/lib/main.dart
@@ -8,16 +8,22 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: FirebaseOptions(
-        apiKey: "AIzaSyAQce9PR40vDxItWDpxB4btKB248a7b39U",
-        authDomain: "bakeryproject-6f924.firebaseapp.com",
-        projectId: "bakeryproject-6f924",
-        storageBucket: "bakeryproject-6f924.appspot.com",
-        messagingSenderId: "868989986714",
-        appId: "1:868989986714:web:d0b3d6ba32ecf1b03b3c23",
-        measurementId: "G-1VNP5SE2X0"),
-  );
+  try {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyAQce9PR40vDxItWDpxB4btKB248a7b39U",
+          authDomain: "bakeryproject-6f924.firebaseapp.com",
+          projectId: "bakeryproject-6f924",
+          storageBucket: "bakeryproject-6f924.appspot.com",
+          messagingSenderId: "868989986714",
+          appId: "1:868989986714:web:d0b3d6ba32ecf1b03b3c23",
+          measurementId: "G-1VNP5SE2X0"),
+    );
+  } on FirebaseException catch (e) {
+    if (e.code != 'duplicated-app') {
+      rethrow;
+    }
+  }
   runApp(MyApp());
 }
 
